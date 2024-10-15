f2ee08753115de1aac5b5980278573566aa36a4b
ElManaa <elmanaahoussem@gmail.com>
2024-10-15 17:59:28 +0200

    adding loader

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function sendRequest(event) {
-    event.preventDefault(); // Prevent form from submitting in the traditional way
-
+    event.preventDefault();  
+    document.getElementById("bttnsubmit").style.display = "none"; 
+    document.getElementById("loader").style.display = "inline-block"; 
+    
     const text = document.getElementById("myprompt").value;
     const url = `https://hook.eu2.make.com/ns2ss2h7xcbvoz4s3hj93nguc9b98u1c?userPrompt=${encodeURIComponent(
       text
@@ -26,6 +28,8 @@ function sendRequest(event) {
         const container = document.getElementById('iframeContainer');
         container.innerHTML=""
         container.appendChild(iframe);
+        document.getElementById("bttnsubmit").style.display = "inline"; 
+        document.getElementById("loader").style.display = "none"; 
       
       })
       .catch((error) => {
